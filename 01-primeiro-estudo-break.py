9d02ca522d187849e3212d05cc5630adf7075c57
DanielSR1 <danielsrcontato@gmail.com>
2022-03-14 15:56:26 -0300

    break

---
+++ 01-primeiro-estudo-break.py
@@ -0,0 +1,10 @@
+soma=0
+print('DIGITE 999 PARA PARAR')
+while True:
+    n=int(input('Digite um número :'))
+    if n ==999:
+        break
+    soma=soma+n
+print(f'a soma entre os número digitados é {soma}')
+
+
